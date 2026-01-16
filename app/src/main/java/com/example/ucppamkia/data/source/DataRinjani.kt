d9b8c049e0b7584615ab9f031c074b61597cd6f7
kiieeaa <kiyaadinda5@gmail.com>
2026-01-16 20:19:41 +0700

    Entitas untuk menyimpan data rute pendakian di database

---
+++ app/src/main/java/com/example/ucppamkia/data/source/DataRinjani.kt
@@ -0,0 +1,7 @@
+package com.example.ucppamkia.data.source
+
+object DataRinjani {
+    val routes = listOf("Via Sembalun", "Via Senaru", "Via Torean", "Via Aik Berik", "Via Timbanuh", "Via Tete Batu")
+    val daftarJenisId = listOf("KTP", "Paspor", "KTM", "NIK")
+    val nationalities = listOf("Indonesia", "Asing (WNA)")
+}
